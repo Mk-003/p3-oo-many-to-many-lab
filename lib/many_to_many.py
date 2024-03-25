161dce32d9170931a8b67ebcb6c82f60268d68c0
joseph <michealkinuthia.m.k@gmail.com>
2024-03-25 12:28:21 +0300

    done

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,5 +1,28 @@
 class Author:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.contracts_list = []
+    
+    def contracts(self):
+        return self.contracts_list
+    
+    def books(self):
+        related_books = []
+        for contract in self.contracts_list:
+            related_books.append(contract.book)
+        return related_books
+    
+    def sign_contract(self, book, date, royalties):
+        new_contract = Contract(book, self, date, royalties)
+        self.contracts_list.append(new_contract)
+        return new_contract
+    
+    def total_royalties(self):
+        total = 0
+        for contract in self.contracts_list:
+            total += contract.royalties
+        return total
+
 
 
 class Book:
@@ -7,4 +30,15 @@ class Book:
 
 
 class Contract:
-    pass
+    contract_list = []
+
+    def __init__(self, book, author, date, royalties):
+        self.book = book
+        self.author = author
+        self.date = date
+        self.royalties = royalties
+        Contract.contract_list.append(self)
+    
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.contract_list if contract.date == date]
